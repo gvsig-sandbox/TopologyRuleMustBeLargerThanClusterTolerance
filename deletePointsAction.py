de37884c582ccd4fa7ac2fc4ad97c434e2bf44bd
hecnita <hectorth23@gmail.com>
2019-08-13 18:18:33 +0200

    multigeometry code

--- deletePointsAction.py
+++ deletePointsAction.py
@@ -5,6 +5,7 @@ from gvsig import geom
 from gvsig.geom import *
 from org.gvsig.fmap.geom import GeometryLocator
 from org.gvsig.fmap.geom import GeometryManager
+from org.gvsig.fmap.geom import GeometryUtils
 from org.gvsig.topology.lib.spi import AbstractTopologyRuleAction
 import sys
 
@@ -44,49 +45,59 @@ class DeletePointsAction(AbstractTopologyRuleAction):
       subtype = lineToFix.getGeometryType().getSubType()
       fixedLine = geoManager.createLine(subtype)
 
-      for i in range(0, lineToFix.getNumVertices()):
-        vertex1 = lineToFix.getVertex(i)
-        point = createPoint(D2, vertex1.getX(), vertex1.getY())
-        if tolerance != 0:
-          vertex1Tolerance = point.buffer(tolerance)
-        else:
-          vertex1Tolerance = point
-        for j in range(0, lineToFix.getNumVertices()):
-          vertex2 = lineToFix.getVertex(j)
-          otherPoint = createPoint(D2, vertex2.getX(), vertex2.getY())
-          if i==j:
-            print "i, j", i, j
-            continue
+      def geometryToModify(lineToFix):
+        for i in range(0, lineToFix.getNumVertices()):
+          vertex1 = lineToFix.getVertex(i)
+          point = createPoint(D2, vertex1.getX(), vertex1.getY())
+          if tolerance != 0:
+            vertex1Tolerance = point.buffer(tolerance)
           else:
-            print "i, j", i, j
-            if not vertex1Tolerance.intersects(otherPoint):
-              print "The distance is larger than the tolerance"
-              if fixedLine.getNumVertices()==0:
-                fixedLine.addVertex(lineToFix.getVertex(i))
-              else:
-                print "The fixedLine vertices number is different from 0"
-              content = False
-              for k in range(0, fixedLine.getNumVertices()):
-                vertex = fixedLine.getVertex(k)
-                if vertex2 == vertex:
-                  content = True
-                  print "Content is True"
-                else:
-                  print "Content is False"
-              if content == False:
-                print "if content == False"
-                if j != lineToFix.getNumVertices()-1 and j>i:
-                  fixedLine.addVertex(lineToFix.getVertex(j))
-                  break
-                else:
-                  print "vertex not added"
-              else:
-                if j<i:
-                  print "The vertex is already contained in the fixedLine"
-                else:
-                 break
+            vertex1Tolerance = point
+          for j in range(0, lineToFix.getNumVertices()):
+            vertex2 = lineToFix.getVertex(j)
+            otherPoint = createPoint(D2, vertex2.getX(), vertex2.getY())
+            if i==j:
+              print "i, j", i, j
+              continue
             else:
-              print "The distance is less than the tolerance"
+              print "i, j", i, j
+              if not vertex1Tolerance.intersects(otherPoint):
+                print "The distance is larger than the tolerance"
+                if fixedLine.getNumVertices()==0:
+                  fixedLine.addVertex(lineToFix.getVertex(i))
+                else:
+                  print "The fixedLine vertices number is different from 0"
+                content = False
+                for k in range(0, fixedLine.getNumVertices()):
+                  vertex = fixedLine.getVertex(k)
+                  if vertex2 == vertex:
+                    content = True
+                    print "Content is True"
+                  else:
+                    print "Content is False"
+                if content == False:
+                  print "if content == False"
+                  if j != lineToFix.getNumVertices()-1 and j>i:
+                    fixedLine.addVertex(lineToFix.getVertex(j))
+                    break
+                  else:
+                    print "vertex not added"
+                else:
+                  if j<i:
+                    print "The vertex is already contained in the fixedLine"
+                  else:
+                   break
+              else:
+                print "The distance is less than the tolerance"
+
+        return fixedLine
+
+      if GeometryUtils.isSubtype(geom.MULTICURVE, lineToFix.getGeometryType().getType()):
+        for x in range(0, lineToFix.getPrimitivesNumber()):
+          geox = lineToFix.getPrimitiveAt(x)
+          fixedLine = geometryToModify(geox)
+      else:
+        fixedLine = geometryToModify(lineToFix)
 
       feature = feature.getEditable()
       feature.set("GEOMETRY", fixedLine)
